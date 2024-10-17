50364b0fa5948cc25ddda8953fffca8d8ec0ae8d
hangytong <hangytong@gmail.com>
2024-10-17 16:26:50 +0800

    update add coin

--- CmeOI/get_cme_data.py
+++ CmeOI/get_cme_data.py
@@ -9,11 +9,40 @@ def query_data(token):
     data_dict = {'timestamp' : [row[0] for row in data], 'value' : [row[1] for row in data]}
     df = pd.DataFrame(data_dict)
     df['time'] = pd.to_datetime(df['timestamp'], unit = 's')
-    df['difference'] = df['value'].diff()
-    df.loc[0,'difference'] = 0
-    df['difference'] = df['difference'].astype('int64')
-    df['difference'] = df['difference'].apply(lambda x: f"{x:,}")
+    df['difference_Dollar'] = df['value'].diff()
+    df.loc[0,'difference_Dollar'] = 0
+    df['difference_Dollar'] = df['difference_Dollar'].astype('int64')
+    df['difference_Dollar'] = df['difference_Dollar'].apply(lambda x: f"{x:,}")
 
     df = df.iloc[1:].reset_index(drop=True)
 
     return df
+
+def query_data_coin(token):
+    url = f'https://velo.xyz/api/m/cmeOiCoins?product={token}&range=2592000000&resolution=1%20day'
+    response = requests.get(url)
+    data = response.json()['d']
+    data_dict = {'timestamp' : [row[0] for row in data], 'value' : [row[1] for row in data]}
+    df = pd.DataFrame(data_dict)
+    df['time'] = pd.to_datetime(df['timestamp'], unit = 's')
+    df['value'] = df['value'].astype(float)
+    df['difference_Coin'] = df['value'].diff()
+    df.loc[0,'difference_Coin'] = 0
+    df['difference_Coin'] = df['difference_Coin'].astype('int64')
+    df['difference_Coin'] = df['difference_Coin'].apply(lambda x: f"{x:,}")
+
+    df = df.iloc[1:].reset_index(drop=True)
+
+    return df
+
+def combine_df(df_dollar,df_coin):
+    df_dollar['difference_Coin'] = df_coin['difference_Coin']
+
+    return df_dollar
+
+def cme_data(token):
+    df_dollar = query_data(token)
+    df_coin = query_data_coin(token)
+    df_final = combine_df(df_dollar,df_coin)
+
+    return df_final
--- CmeOI/main.py
+++ CmeOI/main.py
@@ -9,7 +9,7 @@ folder_directory = os.getcwd()
 
 def run_main():
     for token in tokens:
-        df = get_cme_data.query_data(token)
+        df = get_cme_data.cme_data(token)
         image_path = get_table.data_to_png(df,folder_directory,token)
         caption = f"CME Future OI for {token}"
         post_telegram.send_photo_telegram(image_path, caption)

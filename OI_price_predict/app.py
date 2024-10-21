825ab4edcc58571f434d8d4d0c205004751d4515
hangytong <hangytong@gmail.com>
2024-10-21 14:59:04 +0800

    update photo

--- OI_price_predict/app.py
+++ OI_price_predict/app.py
@@ -1,6 +1,13 @@
 import main
 import time
 from misc_func import get_tickers, get_top_100_tokens_by_volume
+import post_telegram
+import os
+
+current_dir = os.getcwd()
+init_bot_dir = os.path.join(current_dir,"init_bot.png")
+
+post_telegram.send_photo_telegram(init_bot_dir, f"Initialize OI price prediction bot")
 
 days = 30
 interval = '1h'  #hour = h, day = d

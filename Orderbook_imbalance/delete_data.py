39f605f881a452e58456c87ef63b552bf873370d
hangytong <hangytong@gmail.com>
2024-10-22 17:31:53 +0800

    update

--- Orderbook_imbalance/delete_data.py
+++ Orderbook_imbalance/delete_data.py
@@ -21,15 +21,17 @@ def delete_old_data():
                 rows_to_keep = [header]  # Start with the header
                 
                 for row in reader:
-                    try:
-                        # Parse the timestamp, ignoring the time part for "AVERAGE" entries
-                        row_date = datetime.strptime(row[0].split()[0], "%Y-%m-%d")
-                        if row_date >= cutoff_date:
-                            rows_to_keep.append(row)
-                    except ValueError:
-                        # If there's an error parsing the date, keep the row
+                    if 'AVERAGE' in row[0]:
                         rows_to_keep.append(row)
-            
+                    else:
+                        try:
+                            # Parse the timestamp for non-AVERAGE entries
+                            row_date = datetime.strptime(row[0].split()[0], "%Y-%m-%d")
+                            if row_date >= cutoff_date:
+                                rows_to_keep.append(row)
+                        except ValueError:
+                            # If there's an error parsing the date, keep the row
+                            rows_to_keep.append(row)
             # Write the filtered data back to the file
             with open(file_path, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile)
